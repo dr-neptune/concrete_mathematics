cb190678b4bd2eb38b72aebc73f1285d8249cf5d
dr-neptune <mrose4@worcester.edu>
2021-10-26 09:40:16 -0400

    durty durty fanganails

--- euclids_algorithm.py
+++ euclids_algorithm.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 def euclids_algo(m: int, n: int) -> int:
     if m < 0:
         return 0
@@ -10,3 +12,20 @@ def euclids_algo(m: int, n: int) -> int:
 
 euclids_algo(12, 18)
 euclids_algo(226, 430)
+
+def euclid_numbers(n):
+    vals = {0: 1}
+    i = 1
+    while i <= n:
+        j = 0
+        val = 1
+        while j < i:
+            val *= vals[j]
+            j += 1
+        print("val:\t", val + 1)
+        vals[i] = val + 1
+        i += 1
+    return vals
+
+pprint(euclid_numbers(8))
+pprint(euclid_numbers(10))
